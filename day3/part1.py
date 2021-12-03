88733736c0ee6c7608fcb821328c3ffe19a87306
Bram den Ouden <bramdenouden96@hotmail.com>
2021-12-03 23:49:58 +0100

    day 3 WIP

--- day3/part1.py
+++ day3/part1.py
@@ -1,6 +1,22 @@
 import sys
 
-with open(sys.path[0] + '/test.txt') as f:
-    for line in f:
+length = 12
+values = [0]*length
+gamma =[0]*length
+epsilon=[0]*length
 
-        
+with open(sys.path[0] + '/input.txt') as f:
+    for iter, line in enumerate(f):
+        for i in range(0,length):
+            # print(f"{i}\t{line[i]}")
+            values[i] += int(line[i])
+print(values, iter)     
+for i in range(0, length):
+    gamma[i] = str(int(values[i] > (iter/2)))
+    epsilon[i] = str(int(values[i] < (iter/2)))
+gamma = "".join(gamma)
+epsilon = "".join(epsilon)
+print(f"gamma: {gamma}, eps:{epsilon}") 
+
+power_consumption = int(epsilon,2)*int(gamma,2)
+print(power_consumption)
--- day3/part2.py
+++ day3/part2.py
@@ -1,6 +1,28 @@
-import sys
+# import sys
 
-with open(sys.path[0] + '/test.txt') as f:
-    for line in f:
+# length = 12
 
-        
+# # since both have to be adde, it doesn't matter if i get the name correct
+# def getCommonBit(data):
+#     val = 0
+#     with open(sys.path[0] + '/test.txt') as f:
+#         for iter, line in enumerate(f):
+#             val += int(line[n])
+
+
+
+
+
+#         for i in range(0,length):
+#             values[i] += int(line[i])
+# print(values, iter)     
+# for i in range(0, length):
+#     gamma[i] = str(int(values[i] > (iter/2)))
+#     epsilon[i] = str(int(values[i] < (iter/2)))
+# gamma = "".join(gamma)
+# epsilon = "".join(epsilon)
+# print(f"gamma: {gamma}, eps:{epsilon}") 
+
+# power_consumption = int(epsilon,2)*int(gamma,2)
+# print(power_consumption)
+
