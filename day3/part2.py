a2b9550fb63b9aa0e458c344c9d4bc11ef49454b
Bram <bramdenouden96@hotmail.com>
2021-12-07 00:22:59 +0100

    day 3 pt 2 solved

--- day3/part2.py
+++ day3/part2.py
@@ -1,28 +1,83 @@
-# import sys
+import sys
 
-# length = 12
+# get all data from file
+length = 12
+data =[]
+mostOccuringBit = None
+leastOccuringBit = None
 
-# # since both have to be adde, it doesn't matter if i get the name correct
-# def getCommonBit(data):
-#     val = 0
-#     with open(sys.path[0] + '/test.txt') as f:
-#         for iter, line in enumerate(f):
-#             val += int(line[n])
+with open(sys.path[0] + '/input.txt') as f:
+    ones = 0
+    for i, line in enumerate(f):
+        data.append(line.strip('\n'))
+    moreOnes = 2*ones <= i
+    mostOccuringBit = str(int(moreOnes))
+    leastOccuringBit = str(int(not moreOnes))
 
 
 
+# print(data)
+
+oxy = f"{mostOccuringBit}"
+co2 = f"{leastOccuringBit}"
+# print(f"{oxy[-1]}, {co2[-1]}")
+# val1 = "1"
+
+def getBit(col, filter, param):
+    oneCount = 0
+    zeroCount = 0
+    lastResult = {0:'', 1:''}
+    # print()
+    for line in data:
+        if (line[0:col] == param):
+            # print(f"evaluating string {line}")
+            if(line[col] == '1'):
+                oneCount += 1
+                lastResult[1] = line
+            else:
+                zeroCount += 1
+                lastResult[0] = line
+    # print("-------------------")
+    return (oneCount, zeroCount, lastResult)
+
+def getMostOccuringBit(col, filter):
+    oneCount, zeroCount, lastResult = getBit(col, filter, oxy)
+    temp = oneCount >= zeroCount
+    count = oneCount if temp else zeroCount
+    # print(f"Most occuring: {str(int(temp))}")
+    return count, lastResult[temp], str(int(temp)) 
+
+def getLeastOccuringBit(col, filter):
+    oneCount, zeroCount, lastResult = getBit(col, filter, co2)
+    temp = oneCount < zeroCount
+    count = zeroCount if temp else oneCount
+    # print(f"Least occuring: {str(int(temp))}")
+    return count, lastResult[temp], str(int(temp)) 
 
 
-#         for i in range(0,length):
-#             values[i] += int(line[i])
-# print(values, iter)     
-# for i in range(0, length):
-#     gamma[i] = str(int(values[i] > (iter/2)))
-#     epsilon[i] = str(int(values[i] < (iter/2)))
-# gamma = "".join(gamma)
-# epsilon = "".join(epsilon)
-# print(f"gamma: {gamma}, eps:{epsilon}") 
+def getOxy():
+    global oxy
+    for i in range(1,length):
+        numResults, result, bit = getMostOccuringBit(i, oxy[-1])
+        # print(f"numresults = {numResults}")
+        if(numResults == 1):
+            return result
+        else:
+            oxy += bit
 
-# power_consumption = int(epsilon,2)*int(gamma,2)
-# print(power_consumption)
+def getCo2():
+    global co2
+    for i in range(1,length):
+        numResults, result, bit = getLeastOccuringBit(i, co2[-1])
+        # print(f"numresults = {numResults}")
+        if(numResults == 1):
+            return result
+        else:
+            co2 += bit
+    
+oxyResult = getOxy()
+co2Result = getCo2()
 
+print(f"oxy: {oxy}, {int(oxyResult,2)},\t co2: {co2}, {int(co2Result,2)}")
+print(f"solution: {int(oxyResult,2)*int(co2Result,2)}")
+# print(val1)
