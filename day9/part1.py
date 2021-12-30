fa01993397d27b04badd4c4df685b86bccd86bcb
Bram den Ouden <bramdenouden96@hotmail.com>
2021-12-30 17:32:06 +0100

    part 1 done

--- day9/part1.py
+++ day9/part1.py
@@ -3,7 +3,7 @@ import sys
 hmap = {} # (x, y): value
 lowPoints={}
 
-with open(sys.path[0] + '/test.txt') as f:
+with open(sys.path[0] + '/input.txt') as f:
     for y, line in enumerate(f):
         for x, h in enumerate(line.strip()):
             h = int(h)
@@ -11,59 +11,45 @@ with open(sys.path[0] + '/test.txt') as f:
         sizeX = len(line.strip())
     sizeY = y
 
-print(sizeX, sizeY)
+# print(sizeX, sizeY)
 
 # if the check starts at 0,0 each node only needs to check x+1 and y+1 and then 
 # proceed until a low point is found or a previously checked point
 
 def findLowPoint(x,y):
-    print(f"checking x,y {x},{y}; value:{hmap[(x, y)]['height']}")
-    if hmap[(x, y)]['checked']: # this node has been checked, no action required
-        return False 
+    # print(f"checking x,y {x},{y}; value:{hmap[(x, y)]['height']}")
+    node = hmap.get((x,y), False)
+    if(not node or node['checked']): # this node has been checked, no action required
+        # print("skipping!\n")
+        return
     else:
-        hmap[(x, y)]['checked'] = True
-        valSelf = hmap[(x, y)]['height']
-        valRight = hmap[(x+1, y)]['height']
-        valBelow = hmap[(x, min(sizeY-1, y+1))]['height']
-        print(f"valSelf={valSelf}, valRight={valRight}, valBelow={valBelow}")
+        node['checked'] = True
+        isLowPoint = True
+        for x2 in [x-1, x+1]:
+            newNode = hmap.get((x2,y), False)
+            if(newNode and newNode['height'] <= node['height']):
+                isLowPoint = False
+                findLowPoint(x2, y)
 
-        if (valRight < valSelf and valRight < valBelow ):
-            return findLowPoint(min(x+1, sizeX-2), y) #check value to right
-        elif(valBelow < valSelf):
-            return  findLowPoint(x, min(sizeY-1, y+1)) # check value below
-        elif(lowPoints.get((x-1, y), False) or lowPoints.get((x, y-1), False)): # check if no lower point has already be found to the left
-            return False
-        else:
-            print(f"LowPoint found: x,y {x},{y}; value:{valSelf}")
-            lowPoints[(x, y)]=valSelf
-            hmap[(x+1, y)]['checked'] = True
-            hmap[(x, y+1)]['checked'] = True
-            hmap[(max(0,x-1), y)]['checked'] = True
-            hmap[(x, max(0,y-1))]['checked'] = True
-            return True
+        for y2 in [y-1, y+1]:
+            newNode = hmap.get((x,y2), False)
+            if(newNode and newNode['height'] <= node['height']):
+                isLowPoint = False
+                findLowPoint(x, y2)
 
+        if(isLowPoint):
+            print(f"-- LOWPOINT -- x, y, h = {x}, {y}, {node['height']}")
+            lowPoints[(x,y)] = node['height']
 
-
-def test(x,y):
-    for y2 in [max(0, y-1), min(y+1, sizeY-1)]:
-        if y != y2:
-            pass # check if xy is larger than x2y2
-        
-    for x2 in [max(0, x-1), min(x+1, sizeX-1)]:
-        if x != x2:
-            pass #check if xy is larger than x2y2
-
-
-
-
-
-
-
+        # print()
 
 for y in range(0, sizeY-1):
     for x in range(0, sizeX-1):
-        print(x,y)
+        # print(x,y)
         if(not hmap[(x, y)]['checked']):
             findLowPoint(x,y)
 
-[print(p) for p in lowPoints.items()]
+# [print(p) for p in lowPoints.items()]  
+print(len(lowPoints))   
+risks = [val+1 for key, val in lowPoints.items()]
+print(f"risk sum: {sum(risks)}")     
--- day9/part2.py
+++ day9/part2.py
@@ -1,6 +1,55 @@
 import sys
 
-with open(sys.path[0] + '/test.txt') as f:
-    for line in f:
+hmap = {} # (x, y): value
+lowPoints={}
 
-        
+with open(sys.path[0] + '/input.txt') as f:
+    for y, line in enumerate(f):
+        for x, h in enumerate(line.strip()):
+            h = int(h)
+            hmap[(x, y)] = {"height": h, "checked": False}
+        sizeX = len(line.strip())
+    sizeY = y
+
+# print(sizeX, sizeY)
+
+# if the check starts at 0,0 each node only needs to check x+1 and y+1 and then 
+# proceed until a low point is found or a previously checked point
+
+def findLowPoint(x,y):
+    # print(f"checking x,y {x},{y}; value:{hmap[(x, y)]['height']}")
+    node = hmap.get((x,y), False)
+    if(not node or node['checked']): # this node has been checked, no action required
+        # print("skipping!\n")
+        return
+    else:
+        node['checked'] = True
+        isLowPoint = True
+        for x2 in [x-1, x+1]:
+            newNode = hmap.get((x2,y), False)
+            if(newNode and newNode['height'] <= node['height']):
+                isLowPoint = False
+                findLowPoint(x2, y)
+
+        for y2 in [y-1, y+1]:
+            newNode = hmap.get((x,y2), False)
+            if(newNode and newNode['height'] <= node['height']):
+                isLowPoint = False
+                findLowPoint(x, y2)
+
+        if(isLowPoint):
+            print(f"-- LOWPOINT -- x, y, h = {x}, {y}, {node['height']}")
+            lowPoints[(x,y)] = node['height']
+
+        # print()
+
+for y in range(0, sizeY-1):
+    for x in range(0, sizeX-1):
+        # print(x,y)
+        if(not hmap[(x, y)]['checked']):
+            findLowPoint(x,y)
+
+# [print(p) for p in lowPoints.items()]  
+print(len(lowPoints))   
+risks = [val+1 for key, val in lowPoints.items()]
+print(f"risk sum: {sum(risks)}")     
