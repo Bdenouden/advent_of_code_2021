b34223e4535509f8533472e1365255e695b53768
Bram den Ouden <bramdenouden96@hotmail.com>
2022-01-07 20:19:35 +0100

    day done

---
+++ day13/part2.py
@@ -0,0 +1,62 @@
+import sys
+
+
+dots = []
+folds = []
+with open(sys.path[0] + '/input.txt') as f:
+    readingCoordinates = True
+    for line in f:
+        if(line == '\n'):
+            readingCoordinates = False
+        elif(readingCoordinates):
+            x, y = (int(val) for val in line.strip().split(','))
+            dots.append((x, y))
+        else:  # reading instructions
+            temp = line.strip().split('=')
+            folds.append({'axis': temp[0][-1], 'line': int(temp[1])})
+
+
+def foldUp(line):
+    newDots = set()
+    for x, y in dots:
+        if(y > line):
+            newDots.add((x, abs(y-2*line)))
+        else:
+            newDots.add((x, y))
+    return list(newDots)
+
+def foldLeft(line):
+    newDots = set()
+    for x, y in dots:
+        if(x < line):
+            pass
+            newDots.add((abs(line-x-1), y))
+        elif(x > line):
+            newDots.add((x-line-1, y))
+    return list(newDots)
+
+
+def printGrid(dots):
+    maxX, maxY = 0, 0
+    for x, y in dots:
+        maxX = x if x > maxX else maxX
+        maxY = y if y > maxY else maxY
+    grid = [[' ' for _ in range(maxX + 1)] for _ in range(maxY+1)]
+    for x, y in dots:
+        grid[y][abs(x-maxX)] = '#' # X adjustment to fix mirrored effect
+    content = "\n".join(["".join(line) for line in grid])
+    with open(sys.path[0] + "/output.txt", "w+") as f:
+        f.write(content)
+
+
+
+printGrid(dots)
+for fold in folds:
+    print(fold)
+    if fold['axis'] == 'y':
+        dots = foldUp(fold['line'])
+    else:
+        dots = foldLeft(fold['line'])
+        printGrid(dots)
+
+printGrid(dots)
